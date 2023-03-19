02a2f073d49dfcc3dbd38742e14fe00ec139b5b4
mkpouto <mkpoutoobot321@gmail.com>
2023-03-19 14:32:38 -0700

    task 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,8 @@ int main(void)
 			if (i < j && i != j)
 			{
 				putchar(i + '0');
-				putchar(j = '0');
-				if (i+j != 17)
+				putchar(j + '0');
+				if (i + j != 17)
 				{
 					putchar(',');
 					putchar(' ');
