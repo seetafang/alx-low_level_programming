6adbfb5eb2a29d5d17d839934d34cf27cd39468c
mkpouto <mkpoutoobot321@gmail.com>
2023-03-25 17:23:32 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ * Return: 1 for alphabetic character and 0 for any thing else
+ */
+
+
+int _isalpha(int c)
+
+{
+
+if ((c >= 65 && c <= 97)  || (c >= 97 && c <= 122))
+{
+
+return (1);
+	}
+return (0);
+}
