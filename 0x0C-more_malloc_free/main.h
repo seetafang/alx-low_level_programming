a6dcac538e2f51a1bb5886b8091d28bd8c1fabf8
seetafang <mkpoutoobot321@gmail.com>
2023-06-19 15:47:12 -0700

    task 4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,7 +12,7 @@ int is_digit(char *s);
 void errors(void);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
-void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 
