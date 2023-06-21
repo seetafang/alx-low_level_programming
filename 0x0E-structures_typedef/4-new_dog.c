689cf72eabd2a124c2f5d15d8e7740bb5c304477
seetafang <mkpoutoobot321@gmail.com>
2023-06-20 18:49:22 -0700

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,38 +12,40 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    int nameLength, ownerLength;
-    dog_t *newDog;
+	int nameLength, ownerLength;
+	dog_t *newDog;
 
-    if (name == NULL || owner == NULL)
-        return NULL;
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-    nameLength = strlen(name);
-    ownerLength = strlen(owner);
+	nameLength = strlen(name);
+	ownerLength = strlen(owner);
 
-    newDog = (dog_t *)malloc(sizeof(dog_t));
-    if (newDog == NULL)
-        return NULL;
+	newDog = (dog_t *)malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
 
-    newDog->name = (char *)malloc((nameLength + 1) * sizeof(char));
-    if (newDog->name == NULL) {
-        free(newDog);
-        return NULL;
-    }
+	newDog->name = (char *)malloc((nameLength + 1) * sizeof(char));
+			if (newDog->name == NULL)
+				{
+					free(newDog);
+					return (NULL);
+				}
 
-    newDog->owner = (char *)malloc((ownerLength + 1) * sizeof(char));
-    if (newDog->owner == NULL) {
-        free(newDog->name);
-        free(newDog);
-        return NULL;
-    }
+	newDog->owner = (char *)malloc((ownerLength + 1) * sizeof(char));
+			if (newDog->owner == NULL)
+				{
+					free(newDog->name);
+					free(newDog);
+					return (NULL);
+				}
 
-    strncpy(newDog->name, name, nameLength);
-    newDog->name[nameLength] = '\0';
-    strncpy(newDog->owner, owner, ownerLength);
-    newDog->owner[ownerLength] = '\0';
+	strncpy(newDog->name, name, nameLength);
+	newDog->name[nameLength] = '\0';
+	strncpy(newDog->owner, owner, ownerLength);
+	newDog->owner[ownerLength] = '\0';
 
-    newDog->age = age;
+	newDog->age = age;
 
-    return newDog;
+	return (newDog);
 }
